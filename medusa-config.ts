ca9caa670e5a20e02a21f9759eccd52d8eea0723
juliusgrafingelheim-ui <julius.graf.ingelheim@gmail.com>
2026-03-12 08:24:42 +0000

    update

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,16 +1,20 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
-module.exports = defineConfig({
+export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+      storeCors: process.env.STORE_CORS || "https://giradi.vercel.app",
+      adminCors: process.env.ADMIN_CORS || "https://giradi-backend.onrender.com",
+      authCors: process.env.AUTH_CORS || "https://giradi.vercel.app",
+    },
+    jwtSecret: process.env.JWT_SECRET || "supersecret",
+    cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+  },
+  admin: {
+    disable: false,
+  },
 })
